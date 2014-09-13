01ce8c499f0a57bc82bbd1dc86bab94d19c93498
aaronsamuel137 <aaron.davis.samuel@gmail.com>
2014-09-13 15:33:47 -0600

    c version

--- chatServer.c
+++ chatServer.c
@@ -1,6 +1,11 @@
+// #define _XOPEN_SOURCE_EXTENDED 1
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <stdio.h>
+#include <string.h>
+// #include <string>
+// #include <cstdlib>
 
 #include <sys/errno.h>
 #include <stdarg.h>
@@ -10,6 +15,7 @@
 extern int errno;
 
 int errexit(const char *format, ...);
+int updSocket(const char *portnum);
 
 int main(int argc, char**argv)
 {
@@ -19,18 +25,46 @@ int main(int argc, char**argv)
     char mesg[MESSAGE_LENGTH];
     char *portnum = "32000";
 
+    char roomName[24];
+    char *name;
+
     sockfd = updSocket(portnum);
 
     for (;;)
     {
         len = sizeof(cliaddr);
         n = recvfrom(sockfd, mesg, MESSAGE_LENGTH, 0, (struct sockaddr *)&cliaddr, &len);
+
+        if (strncmp("Start ", mesg, 6) == 0)
+        {
+            printf("Got Start command\n");
+            strcpy(roomName, mesg);
+            name = strtok(roomName, " ");
+            name = strtok(NULL, "\0");
+            printf("name: %s\n", name);
+            strncpy(mesg, name, strlen(name));
+            // n = strlen(mesg);
+        }
+        else if (strncmp("Find ", mesg, 5) == 0)
+        {
+            printf("Find\n");
+        }
+        else if (strncmp("Terminate ", mesg, 10) == 0)
+        {
+            printf("Terminate\n");
+        }
+        else
+        {
+            strcpy(mesg, "Invalid command. Commands must beign with Start, Find or Terminate\n\0");
+        }
+        n = strlen(mesg);
         sendto(sockfd, mesg, n, 0, (struct sockaddr *)&cliaddr, sizeof(cliaddr));
-        printf("-------------------------------------------------------\n");
-        mesg[n] = 0;
-        printf("Received the following:\n");
-        printf("%s", mesg);
-        printf("-------------------------------------------------------\n");
+
+        // printf("-------------------------------------------------------\n");
+        // mesg[n] = 0;
+        // printf("Received the following:\n");
+        // printf("%s", mesg);
+        // printf("-------------------------------------------------------\n");
     }
 }
 
@@ -74,7 +108,7 @@ int updSocket(const char *portnum)
     if (bind(s, (struct sockaddr *)&sin, sizeof(sin)) < 0)
     {
         fprintf(stderr, "can't bind to %s port: %s; Trying other port\n", portnum, strerror(errno));
-        sin.sin_port=htons(0); /* request a port number to be allocated by bind */
+        sin.sin_port = htons(0); /* request a port number to be allocated by bind */
         if (bind(s, (struct sockaddr *)&sin, sizeof(sin)) < 0)
             errexit("can't bind: %s\n", strerror(errno));
         else
