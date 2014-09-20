8b75a5ba5fdb8c4180a37519a432be16834c3b2d
aaronsamuel137 <aaron.davis.samuel@gmail.com>
2014-09-20 11:27:47 -0600

    rename files according to spec

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-g++ -D_DARWIN_UNLIMITED_SELECT=1 -o client chatClient.cpp Reader.cpp
-g++ -D_DARWIN_UNLIMITED_SELECT=1 -o server chatServer.cpp Reader.cpp
+g++ -D_DARWIN_UNLIMITED_SELECT=1 -o chat_client chat_client.cpp Reader.cpp
+g++ -D_DARWIN_UNLIMITED_SELECT=1 -o chat_coordinator chat_coordinator.cpp Reader.cpp
 g++ -D_DARWIN_UNLIMITED_SELECT=1 -o chat_server chat_server.cpp Reader.cpp
